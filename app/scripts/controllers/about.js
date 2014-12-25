511430f8cb276db9923c7ee4f66d22c2b69de94f
Ariel Kirkwood <ariel.kirkwood@me.com>
2014-12-24 23:29:48 -0500

    fresh yeoman angular install
    Includes angular-animate.js, angular-resource.js, angular-route.js,
    angular-sanitize.js, and angular-touch.js

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spacePlannerClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the spacePlannerClientApp
+ */
+angular.module('spacePlannerClientApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spacePlannerClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the spacePlannerClientApp
+ */
+angular.module('spacePlannerClientApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
